3a3c687afb589d63743460795b36e61eee0c9a9c
sudhamsuanil <55593152+sudhamsuanil@users.noreply.github.com>
2019-09-27 20:59:09 +0530

    Added few more test cases

--- PMP.UnitTests/UnitTests.cs
+++ PMP.UnitTests/UnitTests.cs
@@ -74,5 +74,91 @@ namespace PMP.UnitTests
             Assert.IsNull(contentResult);
         }
 
+        [Test]
+        public void DeleteProject_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.DeleteProject(1);
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+        [Test]
+        public void DeleteUser_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.DeleteUser(1);
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+
+        [Test]
+        public void AddUser_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.AddUser(new User { Task_ID = "1", Employee_ID="1", FirstName= "AB",Lastname="CD", project_ID="1" });
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+
+        [Test]
+        public void AddTask_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.AddTask(new ProjectManagmenPortal.API.Task { parent_id = "1", project_ID ="1", TaskName = "TestCase", Start_Date = DateTime.Now, End_Date = DateTime.Now.AddDays(1), priority = 15 });
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+        [Test]
+        public void AddParentTask_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.AddParentTask(new ParentTask { parent_task = "ParentTest" });
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+        [Test]
+        public void AddProject_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.AddProject(new Project { ProjectName = "TestProj", priority = 0, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1) });
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+        [Test]
+        public void UpdateStatusOfTask_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            IHttpActionResult actionResult = ic.UpdateStatusOfTask(1);
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+        [Test]
+        public void UpdateProject_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            Project proj = new Project{ ProjectName = "TestProj", priority = 0, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1) };
+            IHttpActionResult actionResult = ic.UpdateProject(proj);
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+        [Test]
+        public void UpdateUser_test()
+        {
+            ProjectManagmentController ic = new ProjectManagmentController();
+            User proj = new User { Task_ID = "1", Employee_ID = "1", FirstName = "AB", Lastname = "EF", project_ID = "1" };
+            IHttpActionResult actionResult = ic.UpdateUser(proj);
+            var contentResult = actionResult as OkResult;
+            Assert.IsNotNull(contentResult);
+
+        }
+
     }
 }
